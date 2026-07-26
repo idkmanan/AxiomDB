f2cedf5096417459af9ad2a104baeb9cbc60b205
idkmanan <mananagg2004@gmail.com>
2026-07-26 20:49:09 +0530

    CI/CD: Implemented CI/CD and github actions

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/health', (req,res)=>{
-  res.status(200).json({ status: 'Ok', timestamp: new Date().toISOString(), uptime: process.uptime()});
+  res.status(200).json({ status: 'OK', timestamp: new Date().toISOString(), uptime: process.uptime()});
 });
 
 app.get('/api', (req, res)=>{
@@ -34,5 +34,9 @@ app.get('/api', (req, res)=>{
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', usersRoutes);
+
+app.use((req,res) => {
+  res.status(404).json({error: "Route not found"});
+})
 
 export default app;
